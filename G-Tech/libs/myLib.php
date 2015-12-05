d7f204e472fd11682957aaed9b4522fada0c33ce
Louri <louri@correo.ugr.es>
2015-12-05 14:36:48 +0100

    contraseña aleatoria
    incluida la función randomPassword que genera una contraseña aleatoria
    de 6 dígitos

--- G-Tech/libs/myLib.php
+++ G-Tech/libs/myLib.php
@@ -38,4 +38,16 @@ function envioCorreo($correo, $asunto, $contenido){
 
   $sendgrid->send($email);
 }
+
+function randomPassword() {
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array();
+    $alphaLength = strlen($alphabet) - 1;
+    for ($i = 0; $i < 6; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass);
+}
+
 ?>
