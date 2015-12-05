84a18854434c6c2945531eb33f5be5e53f79bb95
jdanielsv <dasavi@correo.ugr.es>
2015-12-05 12:55:15 +0100

    Cambion script cambiarPass
    Cambio de la consulta sql ya que se no se elegía un usuario al que
    cambiarle la contraseña.

--- G-Tech/scripts/cambiarPass.php
+++ G-Tech/scripts/cambiarPass.php
@@ -9,7 +9,7 @@ if (!isset($_SESSION['login'])) {
 $pass = md5($_POST['pass']);
 
 $conexion = dbConnect();
-$sql = "UPDATE usuario SET pass=$pass";
+$sql = "UPDATE usuario SET pass=$pass WHERE usuario.login=$_SESSION['login'];
 $resultado = mysqli_query($conexion, $sql);
 mysqli_close($conexion);
 
