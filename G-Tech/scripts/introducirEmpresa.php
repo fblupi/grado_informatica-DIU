693e5399889978625cc42cb4d3094d87135a86e0
jdanielsv <dasavi@correo.ugr.es>
2015-12-09 16:47:46 +0100

    Script para añadir una nueva empresa.

---
+++ G-Tech/scripts/introducirEmpresa.php
@@ -0,0 +1,85 @@
+<?php
+
+include_once "../libs/myLib.php";
+
+if (!isset($_SESSION['login'])) {
+    session_start();
+}
+
+$conexion = dbConnect();
+
+$login=$_SESSION['login'];
+$sqlLoginId="SELECT id FROM usuario where login=$login";
+$resultadoIdLogin = mysqli_query($conexion, $sqlLoginId);
+
+$CIF = $_POST['CIF'];
+$nombre = $_POST['nombre'];
+$direccion = $_POST['direccion'];
+$telefono = $_POST['telefono'];
+$fax = "";
+$descripcion = $_POST['descripcion'];
+$representante = $_POST[''];
+$imagen = "assets/img/oslugr.png";
+$sala = "";//Las salas entiendo que las cogerá desde la interfaz de registrar empresa que se encargará de recogerlas de la base de datos.
+$fechaInicio = "";
+$fechaFin = "";
+
+if (!empty($_POST['fax'])) {
+  $fax = $_POST['fax'];
+}
+if (!empty($_POST['sala'])) {
+  $codigoPostal = $_POST['sala'];
+}
+if (!empty($_POST['fechaInicio'])) {
+  $codigoPostal = $_POST['fechaInicio'];
+}
+if (!empty($_POST['fechaInicio'])) {
+  $codigoPostal = $_POST['fechaInicio'];
+}
+if (!empty($_POST['fechaFin'])) {
+  $codigoPostal = $_POST['fechaFin'];
+}
+
+$subidaCorrecta = false;
+if (isset($_FILES['imagen']) && $_FILES['imagen']['name']) {
+  if ($_FILES['imagen']['error'] > 0) {
+    salir("Ha ocurrido un error en la carga de la imagen", -2);
+  } else {
+    $extensiones = array("image/jpg", "image/jpeg", "image/png");
+    $limite = 4096;
+    if (in_array($_FILES['imagen']['type'], $extensiones) && $_FILES['imagen']['size'] < $limite * 1024) {
+      $foldername = "assets/img/users";
+      $foldermkdir = "../" . $foldername;
+      if (!is_dir($foldermkdir)) {
+        mkdir($foldermkdir, 0777, true);
+      }
+      $extension = "." . split("/", $_FILES['imagen']['type'])[1];
+      $filename = $login . $extension;
+      $ruta = $foldername . "/" . $filename;
+      $rutacrear = $foldermkdir . "/" . $filename;
+      if (!file_exists($rutacrear)) {
+        $subidaCorrecta = @move_uploaded_file($_FILES['imagen']['tmp_name'], $rutacrear);
+        if ($subidaCorrecta) {
+          $imagen = $ruta;
+        }
+      }
+    }
+  }
+}
+
+
+$sql="INSERT INTO empresa (CIF,nombre,direccion,telefono,fax,descripcion,imagen,representante,sala,fechaInicio,fechaFin) VALUES('" . $CIF . "','" . $nombre . "','" . $direccion . "','" . $telefono . "','" . $fax . "','" . $descripcion . "','" . $imagen . "','" . $sala . "','" . $fechaInicio . "','" . $fechaFin . "');";
+
+$resultado = mysqli_query($conexion, $sql);
+mysqli_close($conexion);  
+
+
+if (!$resultado && $subidaCorrecta) {
+  unlink($ruta);
+  salir("La empresa ya existe", -1);
+} else {
+  salir("Se ha registrado la empresa correctamente", 0);
+}
+
+
+?>
