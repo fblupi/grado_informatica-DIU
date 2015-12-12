3b8036c40b43b4b13b30b5f9f72e3f307988f9bf
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-12-12 17:54:36 +0100

    Script promocionar evento (sin probar)

---
+++ G-Tech/scripts/promocionarEvento.php
@@ -0,0 +1,43 @@
+<?php
+
+include_once "../libs/myLib.php";
+
+if (!isset($_SESSION['login'])) {
+    session_start();
+}
+
+$conexion = dbConnect();
+$evento = $_GET['i'];
+$usuario = $_SESSION['id'];
+
+$sqlPermiso = "SELECT empresa, usuario FROM evento WHERE id=$evento";
+$resultadoPermiso = mysqli_query($conexion, $sqlPermiso);
+$filaPermiso = mysqli_fetch_array($resultadoPermiso);
+$representante = $filaPermiso['usuario'];
+if ($filaPermiso['empresa'] != '') {
+  $sqlEmpresa = "SELECT representante FROM empresa WHERE id=$filaPermiso['usuario']";
+  $resultadoEmpresa = mysqli_query($conexion, $sqlEmpresa);
+  $filaEmpresa = mysqli_fetch_array($resultadoEmpresa);
+  $representante = $filaEmpresa['representante'];
+}
+
+if ($representante == $usuario) {
+  $sqlPromocion = "SELECT promocion FROM evento WHERE id=$evento";
+  $resultadoPromocion = mysqli_query($conexion, $sqlPromocion);
+  $filaPromocion = mysqli_fetch_array($resultadoPromocion);
+  $promocion = $filaPromocion['promocion'];
+  $promocion++;
+  $sqlActualizar = "UPDATE evento SET promocion=$promocion WHERE id=$evento";
+  $resultadoActualizar = mysqli_query($conexion, $sqlActualizar);
+  mysqli_close($conexion);
+  if ($resultadoActualizar) {
+    salir("Evento promocionada correctamente", 0):
+  } else {
+    salir("Error al promocionar evento", -1);
+  }
+} else {
+  mysqli_close($conexion);
+  salir("El usuario no tiene permisos", -1);
+}
+
+?>
