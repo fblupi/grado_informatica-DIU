363051864bf3181c8a701f427ea0f00cb64c17ae
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-12-12 23:54:23 +0100

    Chequear usuario correcto en alta y baja de empresa

--- G-Tech/scripts/bajaEmpresa.php
+++ G-Tech/scripts/bajaEmpresa.php
@@ -7,15 +7,27 @@ if (!isset($_SESSION['login'])) {
 }
 
 $conexion = dbConnect();
-$idEmpresa = $_GET['i'];
-$sql = "UPDATE empresa SET baja=1 WHERE id='$idEmpresa'";
-$resultado = mysqli_query($conexion, $sql);
-mysqli_close($conexion);
+$empresa = $_GET['i'];
+$usuario = $_SESSION['id'];
 
-if (!$resultado) {
-  salir("Error al dar de baja", -1);
+$sql = "SELECT representante FROM empresa WHERE id=$empresa";
+$resultado = mysqli_query($conexion, $sql);
+$fila = mysqli_fetch_array($resultado);
+$representante = $fila['representante'];
+
+if ($representante == $usuario) {
+  $sql = "UPDATE empresa SET baja=1 WHERE id='$empresa'";
+  $resultado = mysqli_query($conexion, $sql);
+  mysqli_close($conexion);
+
+  if (!$resultado) {
+    salir("Error al dar de baja", -1);
+  } else {
+    salir("Se ha dado de baja correctamente", 0);
+  }
 } else {
-  salir("Se ha dado de baja correctamente", 0);
+  mysqli_close($conexion);
+  salir("El usuario no tiene permisos", -1);
 }
 
 ?>
