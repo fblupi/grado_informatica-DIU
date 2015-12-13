5e0b401d8bd5a31dc9c59d4e02de5aaa27068850
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-12-13 16:27:00 +0100

    Falla si no se escribe ningún organizador #14

--- G-Tech/scripts/crearEvento.php
+++ G-Tech/scripts/crearEvento.php
@@ -23,71 +23,76 @@ $fechaF = $fechaFin.' '.$horaFin;
 $todoInicio = date('Y-m-d H:i:s', strtotime($fechaI));
 $todoFin = date('Y-m-d H:i:s', strtotime($fechaF));
 
-
+$salir = false;
 if ($_POST['empresa'] != "" && $_POST['usuario'] == ""){
   $empresa = $_POST['empresa'];
   $sql = "INSERT INTO evento (nombre,fechaInicio,fechaFin,precio,plazas,descripcion,requisitos,empresa)
           VALUES ('$nombre','$todoInicio','$todoFin','$precio','$plazas','$descripcion','$requisitos','$empresa')";
-}
-else if ($_POST['empresa'] == "" && $_POST['usuario'] != ""){
+} else if ($_POST['empresa'] == "" && $_POST['usuario'] != ""){
   $usuario = $_POST['usuario'];
   $sql = "INSERT INTO evento (nombre,fechaInicio,fechaFin,precio,plazas,descripcion,requisitos,usuario)
           VALUES ('$nombre','$todoInicio','$todoFin','$precio','$plazas','$descripcion','$requisitos','$usuario')";
+} else {
+  $salir = true;
 }
 
-$conexion = dbConnect();
+if (!$salir) {
+  $conexion = dbConnect();
 
-$resultado = mysqli_query($conexion, $sql);
+  $resultado = mysqli_query($conexion, $sql);
 
-if ($resultado) {
-  if (isset($_FILES['imagen']) && $_FILES['imagen']['name']) {
-    $subidaCorrecta = false;
-    $sql = "SELECT id FROM evento WHERE id=@@Identity";
-    $resultado = mysqli_query($conexion, $sql);
-    $row = mysqli_fetch_array($resultado);
-    $id = $row['id'];
-    if ($_FILES['imagen']['error'] > 0) {
-      salir("Ha ocurrido un error en la carga de la imagen", -1);
-    } else {
-      $extensiones = array("image/jpg", "image/jpeg", "image/png");
-      $limite = 4096;
-      if (in_array($_FILES['imagen']['type'], $extensiones) && $_FILES['imagen']['size'] < $limite * 1024) {
-        $foldername = "assets/img/eventos";
-        $foldermkdir = "../" . $foldername;
-        if (!is_dir($foldermkdir)) {
-          mkdir($foldermkdir, 0777, true);
+  if ($resultado) {
+    if (isset($_FILES['imagen']) && $_FILES['imagen']['name']) {
+      $subidaCorrecta = false;
+      $sql = "SELECT id FROM evento WHERE id=@@Identity";
+      $resultado = mysqli_query($conexion, $sql);
+      $row = mysqli_fetch_array($resultado);
+      $id = $row['id'];
+      if ($_FILES['imagen']['error'] > 0) {
+        salir("Ha ocurrido un error en la carga de la imagen", -1);
+      } else {
+        $extensiones = array("image/jpg", "image/jpeg", "image/png");
+        $limite = 4096;
+        if (in_array($_FILES['imagen']['type'], $extensiones) && $_FILES['imagen']['size'] < $limite * 1024) {
+          $foldername = "assets/img/eventos";
+          $foldermkdir = "../" . $foldername;
+          if (!is_dir($foldermkdir)) {
+            mkdir($foldermkdir, 0777, true);
+          }
+          $extension = "." . split("/", $_FILES['imagen']['type'])[1];
+          $filename = $id . $extension;
+          $ruta = $foldername . "/" . $filename;
+          $rutacrear = $foldermkdir . "/" . $filename;
+          if (!file_exists($rutacrear)) {
+            $subidaCorrecta = @move_uploaded_file($_FILES['imagen']['tmp_name'], $rutacrear);
+            $imagen = $ruta;
+          }
         }
-        $extension = "." . split("/", $_FILES['imagen']['type'])[1];
-        $filename = $id . $extension;
-        $ruta = $foldername . "/" . $filename;
-        $rutacrear = $foldermkdir . "/" . $filename;
-        if (!file_exists($rutacrear)) {
-          $subidaCorrecta = @move_uploaded_file($_FILES['imagen']['tmp_name'], $rutacrear);
-          $imagen = $ruta;
+        if ($subidaCorrecta) {
+          $sql = "UPDATE evento SET imagen='$imagen' WHERE id=$id";
+          $resultado = mysqli_query($conexion, $sql);
+          mysqli_close($conexion);
+          if ($resultado) {
+            salir("Evento añadido correctamente", 0);
+          } else {
+            salir("Ha ocurrido un error con la imagen", -1);
+          }
+        } else { // No se ha subido la imagen
+          mysqli_close($conexion);
+          salir("Ha ocurrido un error subiendo la imagen", -1);
         }
+
       }
-      if ($subidaCorrecta) {
-        $sql = "UPDATE evento SET imagen='$imagen' WHERE id=$id";
-        $resultado = mysqli_query($conexion, $sql);
-        mysqli_close($conexion);
-        if ($resultado) {
-          salir("Evento añadido correctamente", 0);
-        } else {
-          salir("Ha ocurrido un error con la imagen", -1);
-        }
-      } else { // No se ha subido la imagen
-        mysqli_close($conexion);
-        salir("Ha ocurrido un error subiendo la imagen", -1);
-      }
-
+    } else { // No hay imagen
+      mysqli_close($conexion);
+      salir("Evento añadido correctamente", 0);
     }
-  } else { // No hay imagen
+  } else { // Fallo en INSERT
     mysqli_close($conexion);
-    salir("Evento añadido correctamente", 0);
+    salir("Error añadiendo el evento", -1);
   }
-} else { // Fallo en INSERT
-  mysqli_close($conexion);
-  salir("Error añadiendo el evento", -1);
+} else {
+  salir("No se ha itroducido ningún organizador", -1);
 }
 
 ?>
--- G-Tech/scripts/editarEvento.php
+++ G-Tech/scripts/editarEvento.php
@@ -25,77 +25,85 @@ $empresaUsuario = true;
 $todoInicio = date('Y-m-d H:i:s',strtotime($fechaInicio.' '.$horaInicio));
 $todoFin = date('Y-m-d H:i:s',strtotime($fechaFin.' '.$horaFin));
 
+$salir = false;
 if ($_POST['empresa'] != "" && $_POST['usuario'] == ""){
   $empresa=$_POST['empresa'];
 }
 else if ($_POST['empresa'] == "" && $_POST['usuario'] != ""){
   $usuario=$_POST['usuario'];
   $empresaUsuario = false;
+} else {
+  $salir = true;
 }
-$conexion = dbConnect();
 
-if (isset($_FILES['imagen']) && $_FILES['imagen']['name']) {
-  $subidaCorrecta = false;
-  if ($_FILES['imagen']['error'] > 0) {
-    salir("Ha ocurrido un error en la carga de la imagen", -1);
-  } else {
-    $extensiones = array("image/jpg", "image/jpeg", "image/png");
-    $limite = 4096;
-    if (in_array($_FILES['imagen']['type'], $extensiones) && $_FILES['imagen']['size'] < $limite * 1024) {
-      $foldername = "assets/img/eventos";
-      $foldermkdir = "../" . $foldername;
-      if (!is_dir($foldermkdir)) {
-        mkdir($foldermkdir, 0777, true);
-      }
-      $extension = "." . split("/", $_FILES['imagen']['type'])[1];
-      $filename = $evento . $extension;
-      $ruta = $foldername . "/" . $filename;
-      $rutacrear = $foldermkdir . "/" . $filename;
-      if (!file_exists($rutacrear)) {
-        $subidaCorrecta = @move_uploaded_file($_FILES['imagen']['tmp_name'], $rutacrear);
-      } else {
-        unlink($rutacrear);
-        $subidaCorrecta = @move_uploaded_file($_FILES['imagen']['tmp_name'], $rutacrear);
+if (!$salir) {
+  $conexion = dbConnect();
+
+  if (isset($_FILES['imagen']) && $_FILES['imagen']['name']) {
+    $subidaCorrecta = false;
+    if ($_FILES['imagen']['error'] > 0) {
+      salir("Ha ocurrido un error en la carga de la imagen", -1);
+    } else {
+      $extensiones = array("image/jpg", "image/jpeg", "image/png");
+      $limite = 4096;
+      if (in_array($_FILES['imagen']['type'], $extensiones) && $_FILES['imagen']['size'] < $limite * 1024) {
+        $foldername = "assets/img/eventos";
+        $foldermkdir = "../" . $foldername;
+        if (!is_dir($foldermkdir)) {
+          mkdir($foldermkdir, 0777, true);
+        }
+        $extension = "." . split("/", $_FILES['imagen']['type'])[1];
+        $filename = $evento . $extension;
+        $ruta = $foldername . "/" . $filename;
+        $rutacrear = $foldermkdir . "/" . $filename;
+        if (!file_exists($rutacrear)) {
+          $subidaCorrecta = @move_uploaded_file($_FILES['imagen']['tmp_name'], $rutacrear);
+        } else {
+          unlink($rutacrear);
+          $subidaCorrecta = @move_uploaded_file($_FILES['imagen']['tmp_name'], $rutacrear);
+        }
+        if ($subidaCorrecta) {
+          $imagen = $ruta;
+        }
       }
       if ($subidaCorrecta) {
-        $imagen = $ruta;
+
+        //Comprobación de si el representante es usuario o es una empresa
+        if($empresaUsuario)
+          $sql = "UPDATE evento SET nombre='$nombre', fechaInicio='$todoInicio', fechaFin='$todoFin', precio='$precio', plazas='$plazas', descripcion='$descripcion', requisitos='$requisitos', imagen='$imagen', empresa='$empresa' WHERE id=$evento";
+        else
+          $sql = "UPDATE evento SET nombre='$nombre', fechaInicio='$todoInicio', fechaFin='$todoFin', precio='$precio', plazas='$plazas', descripcion='$descripcion', requisitos='$requisitos', imagen='$imagen', usuario='$usuario' WHERE id=$evento";
+
+        $resultado = mysqli_query($conexion, $sql);
+        mysqli_close($conexion);
+        if ($resultado) {
+          salir("Evento editado correctamente", 0);
+        } else {
+          unlink($ruta);
+          salir("Ha ocurrido un error con la imagen", -1);
+        }
+      } else { // No se ha subido la imagen
+        mysqli_close($conexion);
+        salir("Ha ocurrido un error subiendo la imagen", -1);
       }
     }
-    if ($subidaCorrecta) {
+  } else { // No hay imagen
+    //Comprobación de si el representante es usuario o es una empresa
+    if($empresaUsuario)
+      $sql = "UPDATE evento SET nombre='$nombre', fechaInicio='$todoInicio', fechaFin='$todoFin', precio='$precio', plazas='$plazas', descripcion='$descripcion', requisitos='$requisitos', empresa='$empresa' WHERE id=$evento";
+    else
+      $sql = "UPDATE evento SET nombre='$nombre', fechaInicio='$todoInicio', fechaFin='$todoFin', precio='$precio', plazas='$plazas', descripcion='$descripcion', requisitos='$requisitos', usuario='$usuario' WHERE id=$evento";
 
-      //Comprobación de si el representante es usuario o es una empresa
-      if($empresaUsuario)
-        $sql = "UPDATE evento SET nombre='$nombre', fechaInicio='$todoInicio', fechaFin='$todoFin', precio='$precio', plazas='$plazas', descripcion='$descripcion', requisitos='$requisitos', imagen='$imagen', empresa='$empresa' WHERE id=$evento";
-      else
-        $sql = "UPDATE evento SET nombre='$nombre', fechaInicio='$todoInicio', fechaFin='$todoFin', precio='$precio', plazas='$plazas', descripcion='$descripcion', requisitos='$requisitos', imagen='$imagen', usuario='$usuario' WHERE id=$evento";
-
-      $resultado = mysqli_query($conexion, $sql);
-      mysqli_close($conexion);
-      if ($resultado) {
-        salir("Evento editado correctamente", 0);
-      } else {
-        unlink($ruta);
-        salir("Ha ocurrido un error con la imagen", -1);
-      }
-    } else { // No se ha subido la imagen
-      mysqli_close($conexion);
-      salir("Ha ocurrido un error subiendo la imagen", -1);
+    $resultado = mysqli_query($conexion, $sql);
+    mysqli_close($conexion);
+    if ($resultado) {
+      salir("Evento editado correctamente", 0);
+    } else {
+      salir("Error al editar evento", -1);
     }
   }
-} else { // No hay imagen
-  //Comprobación de si el representante es usuario o es una empresa
-  if($empresaUsuario)
-    $sql = "UPDATE evento SET nombre='$nombre', fechaInicio='$todoInicio', fechaFin='$todoFin', precio='$precio', plazas='$plazas', descripcion='$descripcion', requisitos='$requisitos', empresa='$empresa' WHERE id=$evento";
-  else
-    $sql = "UPDATE evento SET nombre='$nombre', fechaInicio='$todoInicio', fechaFin='$todoFin', precio='$precio', plazas='$plazas', descripcion='$descripcion', requisitos='$requisitos', usuario='$usuario' WHERE id=$evento";
-
-  $resultado = mysqli_query($conexion, $sql);
-  mysqli_close($conexion);
-  if ($resultado) {
-    salir("Evento editado correctamente", 0);
-  } else {
-    salir("Error al editar evento", -1);
-  }
+} else {
+  salir("No se ha itroducido ningún organizador", -1);
 }
 
 ?>
