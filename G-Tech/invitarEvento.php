6f68a04608308b2b6ca316dc3954a13cbfc5c6ec
Louri <louri@correo.ugr.es>
2015-12-13 18:17:12 +0100

    interfaz de invitar a evento

---
+++ G-Tech/invitarEvento.php
@@ -0,0 +1,31 @@
+<?php include 'header.php';
+if(!isset($_SESSION['login'])){
+	echo '<script>location.href="inicioSesion.php";</script>';
+}
+$idEvento = $_GET['i'];
+?>
+<section>
+		<h1 class="section-header">Invitar
+		<hr></hr></h1>
+		<article>
+      <form method="POST" action="scripts/invitarEvento.php" data-toggle="validator" role="form">
+				<input type="hidden" name="id" id="id" value="<?php echo $idEvento; ?>">
+        <div class="form-group">
+        <label>Email</label>
+        <input type="text" id="emails" name="emails" class="form-control" placeholder="ejemplo@ejemplo.com,ejemplo2@ejemplo.com" required>
+        <span id="helpBlock" class="help-block">
+          * Puede invitar a tantas personas como desee, escriba los emails separados por comas
+        </span>
+        </div>
+      <input type="submit" class="btn btn-default btnCrearSala" value="Invitar">
+    </form>
+</article>
+</section>
+<script type="text/javascript">
+window.onload = function()
+{
+		document.getElementById("eventos").className = "active menu";
+		$('table').stacktable();
+}
+</script>
+<?php include 'footer.php'; ?>
