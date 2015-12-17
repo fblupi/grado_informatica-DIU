2a58e3a1f43662a91a8bff48a5c2d6712c5b3d8f
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-12-17 20:18:52 +0100

    Script desasignar sala empresa

---
+++ G-Tech/scripts/desasignarSalaEmpresa.php
@@ -0,0 +1,38 @@
+<?php
+
+include_once "../libs/myLib.php";
+
+if (!isset($_SESSION['login'])) {
+  session_start();
+}
+
+$conexion = dbConnect();
+$empresa = $_GET['i'];
+
+$sql = "SELECT sala FROM empresa WHERE id=$empresa";
+$resultado = mysqli_query($conexion, $sql);
+if ($resultado) {
+  $fila = mysqli_fetch_array($resultado);
+  $alquiler = $fila['sala'];
+  $sql = "UPDATE empresa SET sala=NULL WHERE id=$empresa";
+  $resultado = mysqli_query($conexion, $sql);
+  if ($resultado) {
+    $sql = "UPDATE alquiler SET asignada=0 WHERE id=$alquiler";
+    $resultado = mysqli_query($conexion, $sql);
+    mysqli_close($conexion);
+    if ($resultado) {
+      salir("Sala desasignada correctamente", 0);
+    } else {
+      salir("Ha habido un error desasignando la sala volviéndola a poner como disponible", -1);
+    }
+  } else {
+    mysqli_close($conexion);
+    salir("Ha habido un error desasignando la sala de la empresa", -1);
+  }
+  
+} else {
+  mysqli_close($conexion);
+  salir("Ha habido un error buscando la información de la empresa", -1);
+}
+
+?>
